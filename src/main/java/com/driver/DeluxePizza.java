fac5ef14e302c23925357ca639fc3ee29f25ec7d
Mohith Reddy <86471322+Mohith-96-Reddy@users.noreply.github.com>
2023-03-15 10:45:34 +0530

    DeluxePizza.java

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,15 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        // your code goes here
+        // Adding default extra cheese and toppings for deluxe pizza
+        this.addExtraCheese();
+        if (isVeg) {
+            this.addToppings("olives");
+            this.addToppings("mushrooms");
+        } else {
+            this.addToppings("chicken");
+            this.addToppings("sausage");
+            this.addToppings("jalapenos");
+        }
     }
 }
